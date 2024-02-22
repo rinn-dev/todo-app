8e81d11c2518732820fb4e9d9f453fe3b0c83eb0
rinn-dev <mr.htetmyatlinn404@gmail.com>
2024-02-22 15:30:00 +0700

    chore: prettier setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
