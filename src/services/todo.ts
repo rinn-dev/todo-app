90a6b2a6f6dda14139e17e17adcd50ee5d0539bc
rinn-dev <mr.htetmyatlinn404@gmail.com>
2024-02-25 22:17:24 +0700

    feat: loading skeletons added

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -1,11 +1,11 @@
-import { Todos } from '../types/todo';
+import { Todo } from '../types/todo';
 import { api } from './utils/api';
 import { TODO_ENDPOINT } from './utils/endpoints';
 import { TODO } from './utils/tags';
 
 export const todoApi = api.injectEndpoints({
   endpoints: (builder) => ({
-    getTodos: builder.query<Todos, void>({
+    getTodos: builder.query<Todo[], void>({
       query: () => {
         return {
           url: `${TODO_ENDPOINT}`,
