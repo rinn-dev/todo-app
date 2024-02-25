23226037ad2fd72054c5f340262ce1ac51227bad
rinn-dev <mr.htetmyatlinn404@gmail.com>
2024-02-25 12:10:32 +0700

    feat: edit feature for task

--- src/components/tasks/Actions.tsx
+++ src/components/tasks/Actions.tsx
@@ -1,12 +1,20 @@
-import type { FC } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 import './actions.scss';
 
-interface ActionsProps {}
+interface ActionsProps {
+  setIsEditing: Dispatch<SetStateAction<boolean>>;
+}
 
-export const Actions: FC<ActionsProps> = () => {
+export const Actions: FC<ActionsProps> = ({ setIsEditing }) => {
   return (
     <div className="actions">
-      <button className="actions__item">Edit</button>
+      <button
+        type="button"
+        onClick={() => setIsEditing(true)}
+        className="actions__item"
+      >
+        Edit
+      </button>
       <button className="actions__item actions__item-delete">Delete</button>
     </div>
   );
--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -16,32 +16,64 @@ interface TaskProps {
 export const Task: FC<TaskProps> = ({ title, completed }) => {
   const [isCompleted, setIsCompleted] = useState<boolean>(completed);
   const [isActionsOpen, setIsActionsOpen] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [taskTitle, setTaskTitle] = useState<string>(title);
 
   const handleOutsideClick = () => {
     setIsActionsOpen(false);
   };
   const ref = useClickAway<HTMLDivElement>({ callback: handleOutsideClick });
 
+  const handleSave = () => {
+    // Perform save logic
+    setIsEditing(false);
+  };
+
   return (
     <div className="task">
       <div className="task__info">
-        <Checkbox
-          isChecked={isCompleted}
-          onChange={() => setIsCompleted(!isCompleted)}
-        />
-        <span className={classNames({ completed: isCompleted })}>{title}</span>
+        <RenderIf condition={!isEditing}>
+          <Checkbox
+            isChecked={isCompleted}
+            onChange={() => setIsCompleted(!isCompleted)}
+          />
+        </RenderIf>
+        <RenderIf condition={!isEditing}>
+          <span className={classNames({ completed: isCompleted })}>
+            {taskTitle}
+          </span>
+        </RenderIf>
+        <RenderIf condition={isEditing}>
+          <input
+            type="text"
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
+            className="task__info__input"
+          />
+        </RenderIf>
       </div>
       <div ref={ref} className="task__actions">
-        <button
-          type="button"
-          role="menu"
-          onClick={() => setIsActionsOpen(!isActionsOpen)}
-          className="task__actions__trigger"
-        >
-          <img width={24} height={24} src={Dots} alt="dots" />
-        </button>
-        <RenderIf condition={isActionsOpen}>
-          <Actions />
+        <RenderIf condition={!isEditing}>
+          <button
+            type="button"
+            role="menu"
+            onClick={() => setIsActionsOpen(!isActionsOpen)}
+            className="task__actions__trigger"
+          >
+            <img width={24} height={24} src={Dots} alt="dots" />
+          </button>
+          <RenderIf condition={isActionsOpen}>
+            <Actions setIsEditing={setIsEditing} />
+          </RenderIf>
+        </RenderIf>
+        <RenderIf condition={isEditing}>
+          <button
+            type="button"
+            onClick={handleSave}
+            className="task__actions__save"
+          >
+            Save
+          </button>
         </RenderIf>
       </div>
     </div>
