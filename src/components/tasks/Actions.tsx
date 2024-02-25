cf43c9c9dff3ac6183184d8d2af1eb9785666fe3
rinn-dev <mr.htetmyatlinn404@gmail.com>
2024-02-25 18:06:11 +0700

    feat: auto placement for tooltip

--- src/components/tasks/Actions.tsx
+++ src/components/tasks/Actions.tsx
@@ -1,28 +1,36 @@
-import type { Dispatch, FC, SetStateAction } from 'react';
+import {
+  forwardRef,
+  type Dispatch,
+  type SetStateAction,
+  CSSProperties,
+} from 'react';
 import './actions.scss';
 
 interface ActionsProps {
   setIsEditing: Dispatch<SetStateAction<boolean>>;
   handleDelete: () => void;
+  floatingStyles: CSSProperties;
 }
 
-export const Actions: FC<ActionsProps> = ({ setIsEditing, handleDelete }) => {
-  return (
-    <div className="actions">
-      <button
-        type="button"
-        onClick={() => setIsEditing(true)}
-        className="actions__item"
-      >
-        Edit
-      </button>
-      <button
-        type="button"
-        onClick={() => handleDelete()}
-        className="actions__item actions__item-delete"
-      >
-        Delete
-      </button>
-    </div>
-  );
-};
+export const Actions = forwardRef<HTMLDivElement, ActionsProps>(
+  ({ setIsEditing, handleDelete, floatingStyles }, ref) => {
+    return (
+      <div ref={ref} style={floatingStyles} className="actions">
+        <button
+          type="button"
+          onClick={() => setIsEditing(true)}
+          className="actions__item"
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          onClick={() => handleDelete()}
+          className="actions__item actions__item-delete"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+);
